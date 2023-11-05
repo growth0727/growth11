895e534e3ba69b42e7a4ba7f11b2ec7c7ddf0d07
growth0727 <149799441+growth0727@users.noreply.github.com>
2023-11-06 00:19:36 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+system_prompt = """
+あなたは優秀な恋愛アドバイザーです。
+限られた質問内容や時間で、様々な恋愛のアドバイスを提案することができます。
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title(" 🍓恋愛マスターにおまかせあれ🍓")
+st.image("01_love.png")
+st.write("どんなことで悩んでるんですか？")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
